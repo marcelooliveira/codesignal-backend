301d217e71af26fd0c7d662a370db8c089d09e60
marcelooliveira <mclricardo@gmail.com>
2026-06-05 19:16:02 -0300

    feat: solution to p01

--- P01-ProgressiveFilesystem/Solution/IntegerContainer.cs
+++ P01-ProgressiveFilesystem/Solution/IntegerContainer.cs
@@ -1,9 +1,20 @@
+using System.Collections.Generic;
+
 namespace Solution
 {
   public class IntegerContainer : IntegerContainerBase
   {
+    private List<int> _items = new List<int>();
 
-  // TODO: implement interface methods here
+    public override int Add(int value)
+    {
+      _items.Add(value);
+      return _items.Count;
+    }
 
+    public override bool Delete(int value)
+    {
+      return _items.Remove(value);
+    }
   }
 }
